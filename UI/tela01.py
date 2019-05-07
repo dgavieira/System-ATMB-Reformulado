aed208eb18cd09fb7b091b04d64d4e5218e75487
dgavieira <diegoalcantaravieira@gmail.com>
2019-05-06 20:04:39 -0400

    Create Tela Principal
    Commit Inicial. Definição da classe da tela principal.

---
+++ UI/tela01.py
@@ -0,0 +1,12 @@
+#Title: System ATMB Reformulado
+#Organization: Private Project
+#Screen1: Main Screen
+#Description: Main Screen for the User Interface
+#Author: Diego Vieira
+
+from tkinter import *
+
+def tela_main():
+    class MainScreen:
+        def __init__(self, master = None):
+            
