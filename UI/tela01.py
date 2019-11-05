b9969afc68e21eb1f961f13d305c2de22d5bead7
Diego Vieira <dgavieira@users.noreply.github.com>
2019-11-05 17:52:34 -0400

    Rascunhos de Tela
    Rascunhos do front-end das telas 01 e 02

--- UI/tela01.py
+++ UI/tela01.py
@@ -6,7 +6,58 @@
 
 from tkinter import *
 
-def tela_main():
-    class MainScreen:
-        def __init__(self, master = None):
-            
+class MainScreen:
+    def __init__(self, master = None):
+        #primeiro container
+        self.moldura = Frame(master)
+        self.moldura.grid(row = 0, column = 0, rowspan = 2, columnspan = 2, sticky = N)
+
+        #elementos do titulo
+        self.titulo = Label(self.moldura)
+        self.titulo["font"] = ('MS', '30')
+        self.titulo["text"] = "SYSTEM ATMB"
+        self.titulo.pack(side = TOP, fill = X)
+
+        #elementos do subtitulo
+        self.subtitulo = Label(self.moldura)
+        self.subtitulo["font"] = ('MS','12','underline')
+        self.subtitulo["text"] = "Software Educativo de Musicografia Braile"
+        self.subtitulo.pack(side = TOP, fill = X)
+
+        #elementos de botao
+        self.fonte_padrao = ('Arial','10','underline')
+
+        self.botao_historico = Button(master)
+        self.botao_historico["text"] = "Histórico"
+        self.botao_historico["font"] = self.fonte_padrao
+        self.botao_historico.grid(row = 2, column = 0, sticky = NW)
+
+        self.cela_braile = Button(master)
+        self.cela_braile["text"] = "Cela Braille"
+        self.cela_braile["font"] = self.fonte_padrao
+        self.cela_braile.grid(row = 2, column = 1, sticky = NW)
+
+        self.teoria_musical = Button(master)
+        self.teoria_musical["text"] = "Teoria Musical"
+        self.teoria_musical["font"] = self.fonte_padrao
+        self.teoria_musical.grid(row = 3, column = 0, sticky = NW)
+
+        self.musicografia = Button(master)
+        self.musicografia["text"] = "Musicografia Braille"
+        self.musicografia["font"] = self.fonte_padrao
+        self.musicografia.grid(row = 3, column = 1, sticky = NW)
+
+        self.editor = Button(master)
+        self.editor["text"] = "Software Musibraille"
+        self.editor["font"] = self.fonte_padrao
+        self.editor.grid(row = 4, column = 0, sticky = NW)
+
+        self.exerc = Button(master)
+        self.exerc["text"] = "Exercícios"
+        self.exerc["font"] = self.fonte_padrao
+        self.exerc.grid(row = 4, column = 1, sticky = NW)
+
+root = Tk()
+root.title("System ATMB")
+MainScreen(root)
+root.mainloop()
---
+++ UI/tela02.py
@@ -0,0 +1,58 @@
+#Title: System ATMB Reformulado
+#Organization: Private Project
+#Screen2: Histórico da Musicografia Braile
+#Description: Second Screen on the App UX Flow
+#Author: Diego Vieira
+
+from tkinter import *
+
+class ScreenHistorico:
+    def __init__(self, master = None):
+        self.primeiroContainer = Frame(master)
+        self.primeiroContainer.grid(row = 0, column = 0, rowspan = 5, columnspan = 5, sticky = NW)
+
+        self.titulo = Label(self.primeiroContainer)
+        self.titulo["font"] = ('MS', '30')
+        self.titulo["text"] = "Histórico da Musicografia Braille"
+        self.titulo.pack(side=TOP, fill=X)
+
+        self.text = Text(self.primeiroContainer)
+        self.scroll = Scrollbar(self.primeiroContainer)
+        self.scroll["command"] = self.text.yview
+        self.text.configure(yscrollcommand = self.scroll.set)
+        self.text["height"] = 26
+        self.text["width"] = 70
+        self.text["font"] = ('MS','12')
+        self.text.pack(side=LEFT)
+        self.scroll.pack(side=RIGHT, fill=Y)
+
+        self.fonte_padrao = ('Arial', '10')
+
+        self.botao_um = Button(master)
+        self.botao_um["text"] = "Ouvir"
+        self.botao_um["font"] = self.fonte_padrao
+        self.botao_um.grid(row = 1, column = 6, sticky = NW)
+
+        self.botao_dois = Button(master)
+        self.botao_dois["text"] = "Avançar"
+        self.botao_dois["font"] = self.fonte_padrao
+        self.botao_dois.grid(row = 2, column = 6, sticky = NW)
+
+        self.botao_tres = Button(master)
+        self.botao_tres["text"] = "Voltar"
+        self.botao_tres["font"] = self.fonte_padrao
+        self.botao_tres.grid(row = 3, column = 6, sticky = NW)
+
+        self.botao_quatro = Button(master)
+        self.botao_quatro["text"] = "Ouvir Texto Atual"
+        self.botao_quatro["font"] = self.fonte_padrao
+        self.botao_quatro.grid(row = 4, column = 6, sticky = NW)
+
+        self.botao_cinco = Button(master)
+        self.botao_cinco["text"] = "?"
+        self.botao_cinco[""]
+
+root = Tk()
+root.title("Histórico da Musicografia Braille")
+ScreenHistorico(root)
+root.mainloop()
